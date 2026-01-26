3ed85b067fb610e4becd35346e333ba59e02b636
thutrang180688 <thutrang180688@gmail.com>
2026-01-26 12:39:55 +0700

    Fix Firebase API key error

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,9 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// 🔥 DÙNG KEY CHÍNH XÁC TỪ FIREBASE CONSOLE
+// Your web app's Firebase configuration
+// 🔥 KEY CHÍNH XÁC TỪ FIREBASE CONSOLE - SỬA LỖI ĐĂNG NHẬP
 const firebaseConfig = {
   apiKey: "AIzaSyAspK68BLdI14ygZtQfh6gIIVY0L1pV_qE",
   authDomain: "chamcongonline-7df7f.firebaseapp.com",
@@ -13,8 +15,16 @@ const firebaseConfig = {
   appId: "1:80878628372:web:480137f9899e7997aa8101"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+
+// Initialize Cloud Storage and get a reference to the service
 export const storage = getStorage(app);
+
 export default app;
