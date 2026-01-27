be87cc45844801b8301f170faa9bb9cc239f7637
thutrang180688 <thutrang180688@gmail.com>
2026-01-27 15:14:34 +0700

    upicon

---
+++ public/sw.js
@@ -0,0 +1,45 @@
+// Service Worker cho ứng dụng Chấm Công
+const CACHE_NAME = 'chamcong-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
